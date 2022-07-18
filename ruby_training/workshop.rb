71a1f16d35c2cb9e074e26b3bc5888fc17aa0bdd
bluishfluid666 <nguyenmanhkhang0805@gmail.com>
2022-07-18 10:30:46 +0700

    having problems with git diff

--- ruby_training/workshop.rb
+++ ruby_training/workshop.rb
@@ -1,8 +1,9 @@
 def write_hello
-  puts "hello kernel".upcase.reverse
-  puts "kannible"
+  puts "hello kernel".capitalize.reverse
+  puts 1+1
 end
 write_hello
 cars = ["merc", "bentley", "royce", "maserati", "lambo"]
 cars.reverse.each{|c| puts c}
 puts cars.select{|fav_brand| fav_brand == "bentley"}
+puts "hol'up something ain't right"
