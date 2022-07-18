ccb70f6631cab843456674bfb87693d76999f909
bluishfluid666 <nguyenmanhkhang0805@gmail.com>
2022-07-18 10:06:37 +0700

    modify workshop.rb to test git adv cmds

--- ruby_training/workshop.rb
+++ ruby_training/workshop.rb
@@ -1,3 +1,8 @@
 def write_hello
-  puts "hello kernel".reverse
+  puts "hello kernel".upcase.reverse
+  puts "kannible"
 end
+write_hello
+cars = ["merc", "bentley", "royce", "maserati", "lambo"]
+cars.reverse.each{|c| puts c}
+puts cars.select{|fav_brand| fav_brand == "bentley"}
